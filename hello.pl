310b0b71c6c30c1af5a639a4bc55e406ff0a0379
Timothy Millard <millardster@gmail.com>
2016-05-09 22:44:41 -0500

    initial import

---
+++ hello.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/perl
+
+my $sqlite_file = "/home/mill4138/bike.sl";
+
+use strict;
+use HTML::Template;
+use DBI;
+use URI::Query;
+#use Switch;
+use feature "switch";
+use v5.10.1;
+
+use Data::Dumper::Simple;
+
+# open the html template
+my $template = HTML::Template->new(filename => 'template/admin.tmpl');
+
+sub view_initial_checkin_admin() {
+  # Connect to Database
+  my $dbh = DBI->connect("dbi:SQLite:dbname=$sqlite_file","","");
+
+
+  # http://www.perl.com/pub/1999/10/DBI.html
+  my $sth = $dbh->prepare('SELECT * FROM checkin')
+#  my $sth = $dbh->prepare('select person.*, checkin.id from checkin join person ON checkin.person_id = person.person_id')
+                or die "Couldn't prepare statement: " . $dbh->errstr;
+  $sth->execute()
+	or die "Couldn't execute statement: " . $sth->errstr; # Run Query
+      my @bike_table;
+      # Load data into HTML template from Database
+      while (my @data = $sth->fetchrow_array()) {
+	my %params_hash;
+$params_hash{'ZIP'} = $data[2];
+	$params_hash{'CHECKIN_ID'} = $data[1];
+	push(@bike_table, \%params_hash);
+      }
+
+  $template->param(SHOW_MSG => 1); # There is a message to show at top of page.
+  $template->param(MSG => [
+		{MSG_BODY=>"Message one. Perhaps an error here."},
+		{MSG_BODY => "Message Two"}
+  ]);
+  $template->param(BIKE_TABLE => \@bike_table);
+}
+
+my $qq = URI::Query->new($ENV{'QUERY_STRING'});
+my %query = $qq->hash;
+
+for ($query{'view_mode'}) {
+	when (/^initial/) { &view_initial_checkin_admin(); }
+}
+	 &view_initial_checkin_admin();
+
+# send the obligatory Content-Type and print the template output
+print "Content-Type: text/html\n\n", $template->output;
+
+
+#print Dumper(%qq);
+#print $ENV{'QUERY_STRING'};
+
